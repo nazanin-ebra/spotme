792a420fc2b4abdbbf1cdaac820a2c4c877ca3e4
Nazanin Ebrahimi <Nazanin.h.ebrahimi@gmail.com>
2022-07-26 19:18:07 +0300

    tested the gmaps parts of the api, I think I will use nearny results with combination with aws maps after reading about it a bit

---
+++ main/test.py
@@ -0,0 +1,31 @@
+import os
+import googlemaps
+import requests
+from dotenv import load_dotenv
+from datetime import datetime
+
+
+load_dotenv()
+GMAPS_API = os.environ['GMAPS_API']
+gmaps = googlemaps.Client(key=GMAPS_API)
+
+
+# Just testing the various functionalities that I might need in aws and gcp
+l_x ='31.959733'
+l_y = '35.861790'
+l_t ='256 Zahran St'
+
+nearby_url = f"https://maps.googleapis.com/maps/api/place/nearbysearch/json?location={l_x}%2C{l_y}&rankby=distance&key={GMAPS_API}"
+payload={}
+headers = {}
+
+nearby_result = requests.request("GET", nearby_url, headers=headers, data=payload)
+
+
+geocode_result = gmaps.geocode(l_t)
+reverse_geocode_result = gmaps.reverse_geocode(f"{l_x},{l_y}")
+
+
+# print(reverse_geocode_result)
+# print(geocode_result)
+# print(nearby_result.text)
